5a33ff3406c3610645b590b5fc2f3f555dc9cdd6
Anastasiia Belosevich <82367366+abelosev@users.noreply.github.com>
2024-06-12 15:37:24 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../inc/minishell.h"
+#include "../inc/minishell.h"
 
 // void	init_readline(char *line)
 // {
